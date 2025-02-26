dda36bc8dd275230d3cc6c15cb9b5ea05926ed0e
NKDataConv <nicolaskuhaupt@aol.com>
2025-02-26 08:03:26 +0100

    x

--- datenmanagement_wetter.py
+++ datenmanagement_wetter.py
@@ -20,12 +20,12 @@ df["RainTomorrow"] = le.transform(df["RainTomorrow"])
 risk_mm = df["RISK_MM"]
 y = df["RainTomorrow"]
 
-df = df.drop(columns=["RISK_MM", "RainTomorrow", "Date", "WindGustDir", "WindDir9am", "WindDir3pm"])
+df = df.drop(columns=["RISK_MM", "Location", "RainTomorrow", "Date", "WindGustDir", "WindDir9am", "WindDir3pm"])
 
 df = pd.get_dummies(df)
 
 from sklearn.model_selection import train_test_split
 
-x_train, x_test, y_train, y_test = train_test_split(df, y, test_size=0.4, random_state=42, stratify=y)
+x_train, x_test, y_train, y_test, risk_mm_train, risk_mm_test = train_test_split(df, y, risk_mm, test_size=0.4, random_state=42, stratify=y)
 
-x_test, x_vali, y_test, y_vali = train_test_split(x_test, y_test, test_size=0.75, random_state=42, stratify=y_test)
+x_test, x_vali, y_test, y_vali, risk_mm_test, risk_mm_vali = train_test_split(x_test, y_test, risk_mm_test, test_size=0.75, random_state=42, stratify=y_test)
---
+++ regression_wetter.py
@@ -0,0 +1,31 @@
+from datenmanagement_wetter import x_train, x_vali, x_test, risk_mm_vali, risk_mm_train, risk_mm_test
+import pandas as pd
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+
+PARAMS = {"max_depth": 5}
+reg = DecisionTreeRegressor(**PARAMS)
+reg.fit(X=x_train, y=risk_mm_train)
+
+predictions_train = reg.predict(x_train)
+df_pred = pd.DataFrame({'actual': risk_mm_train, 'prediction': predictions_train})
+df_pred["fehler"] = abs(df_pred["actual"] - df_pred["prediction"])
+print("Gesamtfehler Train:", df_pred["fehler"].mean())
+
+mae = mean_absolute_error(risk_mm_train, predictions_train)
+print("Gesamtfehler Train:", mae)
+
+mse = mean_squared_error(risk_mm_train, predictions_train)
+print("MSE Train", mse)
+
+predictions_vali = reg.predict(x_vali)
+mae = mean_absolute_error(risk_mm_vali, predictions_vali)
+print("Gesamtfehler Vali:", mae)
+
+mse = mean_squared_error(risk_mm_vali, predictions_vali)
+print("MSE Vali", mse)
+
+import plotly.graph_objects as go
+fig = go.Figure()
+fig.add_trace(go.Scatter(x=risk_mm_vali, y=predictions_vali, mode='markers'))
+fig.show()
